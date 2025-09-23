9e41fe60df098f857dba80fabc2310b568712ec0
Alexander Borzenkov <ABorzenkov@alfabank.ru>
2025-09-23 21:46:44 +0300

    feat(app): *

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,13 +1,13 @@
-import { build } from "esbuild";
+import { build } from 'esbuild';
 
 build({
-  entryPoints: ["src/index.ts"],
+  entryPoints: ['src/index.ts'],
   bundle: true,
-  platform: "node",
-  target: "node18",
-  outfile: "dist/server.min.js",
+  platform: 'node',
+  target: 'node18',
+  outfile: 'dist/server.min.js',
   sourcemap: true,
-  minify: false
+  minify: true,
 }).catch((err) => {
   console.log(err);
   process.exit(1);
